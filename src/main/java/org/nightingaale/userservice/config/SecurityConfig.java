dec69420b5e2fae175acd1f07b7ea53d9ddf36e8
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-26 22:14:34 +0300

    feature: add security filter

---
+++ src/main/java/org/nightingaale/userservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.nightingaale.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                .requestMatchers("api//v1/openapi-docs/**").permitAll()
+                .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
