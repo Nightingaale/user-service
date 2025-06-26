1b7b379e7ec57406a33d482e4e4f4426351b80ca
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-27 00:23:16 +0300

    feature: add jwt decoder

--- src/main/java/org/nightingaale/userservice/config/SecurityConfig.java
+++ src/main/java/org/nightingaale/userservice/config/SecurityConfig.java
@@ -4,18 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("")
+                .jwsAlgorithm(SignatureAlgorithm.ES256)
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                .requestMatchers("api//v1/openapi-docs/**").permitAll()
+                                .requestMatchers("api/v1/openapi-docs/**").permitAll()
                 .anyRequest().authenticated()
                 )
 
