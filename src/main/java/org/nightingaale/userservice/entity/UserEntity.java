56a7041cee839751768f6b5348bb22f589cbaac2
Nightingaale <shirounightingaleoff@gmail.com>
2025-07-21 22:59:37 +0300

    feature: add new fields

--- src/main/java/org/nightingaale/userservice/entity/UserEntity.java
+++ src/main/java/org/nightingaale/userservice/entity/UserEntity.java
@@ -1,22 +1,43 @@
 package org.nightingaale.userservice.entity;
 
-import jakarta.persistence.PrePersist;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
-@Document(collection = "users-info")
+@Document(collection = "user_info")
+@Getter
+@Setter
 public class UserEntity {
     @Id
+    @JsonProperty("_id")
     private String id;
 
-    @PrePersist
-    public void prePersist() {
-        if (id == null || id.isEmpty()) {
-            id = UUID.randomUUID().toString();
-        }
-    }
+    private String userId;
+
+    @Size(max = 16)
+    private String username;
+
+    @Size(max = 32)
+    private String firstName;
+
+    @Size(max = 32)
+    private String lastname;
+
+    @JsonProperty("user-bio")
+    private List<String> bio = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdInfoAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedInfoAt;
 }
