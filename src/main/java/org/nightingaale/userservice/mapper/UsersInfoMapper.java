e1be3bdca043de51050b99b0cd656934a7f901ef
Nightingaale <shirounightingaleoff@gmail.com>
2025-07-23 00:11:24 +0300

    feature: add mappers

---
+++ src/main/java/org/nightingaale/userservice/mapper/UsersInfoMapper.java
@@ -0,0 +1,14 @@
+package org.nightingaale.userservice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.nightingaale.userservice.dto.UserDto;
+import org.nightingaale.userservice.entity.UserEntity;
+
+@Mapper(componentModel = "spring")
+public interface UsersInfoMapper {
+    @Mapping(target = "createdInfoAt", ignore = true)
+    @Mapping(target = "updatedInfoAt", ignore = true)
+    UserEntity toEntity(UserDto dto);
+    UserDto toDto(UserEntity entity);
+}
