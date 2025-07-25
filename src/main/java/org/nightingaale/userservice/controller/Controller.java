8069b67250d2f14ae47c8e329a2bda8648d13b7c
Nightingaale <shirounightingaleoff@gmail.com>
2025-07-26 02:43:15 +0300

    feature: create logic for first request(get user's uuid from auth-service & put it in mongo)

--- src/main/java/org/nightingaale/userservice/controller/Controller.java
+++ src/main/java/org/nightingaale/userservice/controller/Controller.java
@@ -1,27 +1,36 @@
 package org.nightingaale.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.nightingaale.userservice.dto.UserDto;
 import org.nightingaale.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@Slf4j
 public class Controller {
 
     private final UserService userService;
 
-    @PostMapping("/Info")
-    public ResponseEntity<?> createProfile(@RequestBody UserDto event) {
-        event.setId(UUID.randomUUID().toString());
-        userService.createProfile(event);
-        return ResponseEntity.ok("User's profile information has successfully been created");
+    @PostMapping("/createInfo")
+    public ResponseEntity<?> createProfile(@RequestBody UserDto userDto, @AuthenticationPrincipal Jwt jwt) {
+        try {
+
+            UUID userId = UUID.fromString(jwt.getSubject());
+            userDto.setId(userId.toString());
+
+        } catch (RuntimeException e) {
+            log.error("[Invalid JWT subject: " + e + "]");
+            throw new RuntimeException("[Invalid userId in JWT]");
+        }
+        userService.createProfile(userDto);
+        return ResponseEntity.ok("[User's profile information has successfully been created]");
     }
 }
--- src/main/java/org/nightingaale/userservice/service/UserService.java
+++ src/main/java/org/nightingaale/userservice/service/UserService.java
@@ -7,7 +7,6 @@ import org.nightingaale.userservice.entity.UserEntity;
 import org.nightingaale.userservice.mapper.UsersInfoMapper;
 import org.nightingaale.userservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -17,16 +16,14 @@ public class UserService {
     private final UsersInfoMapper usersInfoMapper;
     private final UserRepository userRepository;
 
-    @Transactional
     public void createProfile(UserDto userDto) {
         try {
             if (userRepository.existsById(userDto.getId())) {
-                log.warn("User with id " + userDto.getId() + " already exists");
+                log.warn("[User with id: " + userDto.getId() + " already exists]");
                 return;
             }
 
             UserDto event = new UserDto();
-            event.setId(userDto.getId());
             event.setUsername(userDto.getUsername());
             event.setFirstName(userDto.getFirstName());
             event.setLastname(userDto.getLastname());
@@ -34,10 +31,10 @@ public class UserService {
 
             UserEntity userEntity = usersInfoMapper.toEntity(userDto);
             userRepository.save(userEntity);
-            log.info("User's info with id " + userDto.getId() + " created");
+            log.info("[User's info with id: " + userDto.getId() + " created]");
         }
-        catch (Exception e) {
-            log.error("User's info cannot be created. Error: " + e.getMessage());
+        catch (RuntimeException e) {
+            log.error("[User's info cannot be created. Error: " + e.getMessage() + "]");
         }
     }
 }
