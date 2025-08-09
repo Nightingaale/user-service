d375cfc0610b884da1964c793d71847b48c64724
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-10 02:12:46 +0300

    feature: add delete request

--- src/main/java/org/nightingaale/userservice/controller/Controller.java
+++ src/main/java/org/nightingaale/userservice/controller/Controller.java
@@ -2,7 +2,8 @@ package org.nightingaale.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.nightingaale.userservice.dto.UserDto;
+import org.nightingaale.userservice.dto.UserProfileDto;
+import org.nightingaale.userservice.event.UserRegistrationEvent;
 import org.nightingaale.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,23 +15,22 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-@Slf4j
 public class Controller {
 
     private final UserService userService;
 
     @PostMapping("/createInfo")
-    public ResponseEntity<?> createProfile(@RequestBody UserDto userDto, @AuthenticationPrincipal Jwt jwt) {
-        try {
-
-            UUID userId = UUID.fromString(jwt.getSubject());
-            userDto.setId(userId.toString());
-
-        } catch (RuntimeException e) {
-            log.error("[Invalid JWT subject: " + e + "]");
-            throw new RuntimeException("[Invalid userId in JWT]");
-        }
-        userService.createProfile(userDto);
+    public ResponseEntity<?> createProfileFromEvent(@RequestBody UserRegistrationEvent event, @AuthenticationPrincipal Jwt jwt) {
+        UUID userId = UUID.fromString(jwt.getSubject());
+        event.setUserId(userId.toString());
+        userService.createProfile(event);
         return ResponseEntity.ok("[User's profile information has successfully been created]");
+    }
+
+    @DeleteMapping("/deleteProfile/{id}")
+    public ResponseEntity<?> deleteProfile(@AuthenticationPrincipal Jwt jwt) {
+        String userId = jwt.getSubject();
+        userService.deleteProfile(userId);
+        return ResponseEntity.ok("[User's profile has been deleted]");
     }
 }
