35f7d2fbd791086f03729729232a3c206b28dd4c
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-09 20:39:50 +0300

    feature: add kafka event

---
+++ src/main/java/org/nightingaale/userservice/event/UserRegistrationEvent.java
@@ -0,0 +1,16 @@
+package org.nightingaale.userservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationEvent {
+    private String correlationId;
+    private String userId;
+    private String email;
+    private String username;
+    private String password;
+}
