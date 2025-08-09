f17c6e6a72cd60ac3e198d2a054fdb628ea32c02
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-09 20:55:26 +0300

    refactor: keycloak url

--- src/main/java/org/nightingaale/userservice/config/SecurityConfig.java
+++ src/main/java/org/nightingaale/userservice/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8080/realms/auth-service/protocol/openid-connect/certs")
+        return NimbusJwtDecoder.withJwkSetUri("http://keycloak:8080/realms/auth-service/protocol/openid-connect/certs")
                 .jwsAlgorithm(SignatureAlgorithm.RS256)
                 .build();
     }
