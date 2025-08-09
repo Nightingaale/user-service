4709dcef75373d9d4ad16785c47f2d24e8291462
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-09 20:40:29 +0300

    feature: new repositories

---
+++ src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
@@ -0,0 +1,9 @@
+package org.nightingaale.userservice.repository;
+
+import org.nightingaale.userservice.entity.UserDataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDataRepository extends JpaRepository<UserDataEntity, Long> {
+}
---
+++ src/main/java/org/nightingaale/userservice/repository/UserProfileRepository.java
@@ -0,0 +1,9 @@
+package org.nightingaale.userservice.repository;
+
+import org.nightingaale.userservice.entity.UserProfileEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends MongoRepository<UserProfileEntity, String> {
+}
