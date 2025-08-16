b704078e31a396356cf57cb55facd5d5f7ea40a5
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-16 17:17:40 +0300

    refactor: long -> string(id)

--- src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
+++ src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDataRepository extends JpaRepository<UserDataEntity, Long> {
+public interface UserDataRepository extends JpaRepository<UserDataEntity, String> {
 }
