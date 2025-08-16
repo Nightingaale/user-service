ce3ed972852c9bf335512d9ff52b7e28ba08ebd9
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-17 02:26:44 +0300

    feature: add kafka listener

---
+++ src/main/java/org/nightingaale/userservice/listener/UserListener.java
@@ -0,0 +1,29 @@
+package org.nightingaale.userservice.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.nightingaale.userservice.event.UserRegistrationEvent;
+import org.nightingaale.userservice.event.UserRemoveEvent;
+import org.nightingaale.userservice.service.UserService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserListener {
+
+    private final UserService userService;
+
+    @KafkaListener(topics = "user-registration", groupId = "user-service", containerFactory = "kafkaListenerContainerFactoryUserRegistered")
+    public void userRegistration(UserRegistrationEvent event) {
+        log.info("[Received user-registration event: " + event + "]");
+        userService.createProfile(event);
+    }
+
+    @KafkaListener(topics = "user-remove", groupId = "user-service", containerFactory = "kafkaListenerContainerFactoryUserRemove")
+    public void userRemoval(UserRemoveEvent event) {
+        log.info("[Received user-remove event: " + event + "]");
+        userService.deleteProfile(event);
+    }
+}
