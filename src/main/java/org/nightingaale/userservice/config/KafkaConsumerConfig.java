4d67f48413201cc0fccb4f8c3f09765762e73c80
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-17 02:25:31 +0300

    feature: add remove consumer

--- src/main/java/org/nightingaale/userservice/config/KafkaConsumerConfig.java
+++ src/main/java/org/nightingaale/userservice/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package org.nightingaale.userservice.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.nightingaale.userservice.event.UserRegistrationEvent;
+import org.nightingaale.userservice.event.UserRemoveEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,11 +43,35 @@ public class KafkaConsumerConfig {
         return new DefaultKafkaConsumerFactory<>(consumerConfig);
     }
 
-
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, UserRegistrationEvent>> kafkaListenerContainerFactoryUserRegistered(
             ConsumerFactory<String, UserRegistrationEvent> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, UserRegistrationEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, UserRemoveEvent> consumerUserRemoveFactory() {
+        Map<String, Object> consumerConfig = new HashMap<>();
+
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+
+        consumerConfig.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        consumerConfig.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        consumerConfig.put(JsonDeserializer.VALUE_DEFAULT_TYPE, UserRemoveEvent.class.getName());
+        consumerConfig.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<>(consumerConfig);
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, UserRemoveEvent>> kafkaListenerContainerFactoryUserRemove(
+            ConsumerFactory<String, UserRemoveEvent> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, UserRemoveEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
     }
