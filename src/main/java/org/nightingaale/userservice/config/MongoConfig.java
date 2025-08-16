49b213c229d908fb6ed928674d3302fa706511d3
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-16 18:52:48 +0300

    feature: add mongo config

---
+++ src/main/java/org/nightingaale/userservice/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package org.nightingaale.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
