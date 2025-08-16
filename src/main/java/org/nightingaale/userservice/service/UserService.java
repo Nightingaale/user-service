ae0925041f7634833b1e86fe6f1afef7bca825e7
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-16 18:53:23 +0300

    refactor: user profile repo

--- src/main/java/org/nightingaale/userservice/service/UserService.java
+++ src/main/java/org/nightingaale/userservice/service/UserService.java
@@ -14,19 +14,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserProfileRepository userRepository;
+    private final UserProfileRepository userProfileRepository;
     private final UserRegistrationMapper userRegistrationMapper;
 
     @KafkaListener(topics = "user-registration", groupId = "user-service", containerFactory = "kafkaListenerContainerFactoryUserRegistered")
     public void createProfile(UserRegistrationEvent event) {
         try {
-            if (userRepository.existsById(event.getUserId())) {
+            if (userProfileRepository.existsById(event.getUserId())) {
                 log.warn("[User with id: " + event.getUserId() + " already exists]");
                 return;
             }
 
             UserProfileEntity userEntity = userRegistrationMapper.fromRegistrationEvent(event);
-            userRepository.save(userEntity);
+            userProfileRepository.save(userEntity);
 
             log.info("[User's profile with id: " + event.getUserId() + "] has been created");
         } catch (RuntimeException e) {
@@ -36,12 +36,12 @@ public class UserService {
 
     public void deleteProfile(String userId) {
         try {
-            if (userRepository.existsById(userId)) {
+            if (userProfileRepository.existsById(userId)) {
                 log.warn("[User with id: " + userId + " does not exist]");
                 throw new RuntimeException("User profile is not found");
             }
 
-            userRepository.deleteById(userId);
+            userProfileRepository.deleteById(userId);
             log.info("[User's info with id: " + userId + "has been deleted]");
         } catch (RuntimeException e) {
             log.error("[User's info with id: " + userId + "] could not be deleted", e);
