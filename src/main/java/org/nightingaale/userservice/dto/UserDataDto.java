0e9293351db002eedfa37698509468b50e2e202d
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-18 21:41:15 +0300

    refactor: data annotation

--- src/main/java/org/nightingaale/userservice/dto/UserDataDto.java
+++ src/main/java/org/nightingaale/userservice/dto/UserDataDto.java
@@ -2,20 +2,13 @@ package org.nightingaale.userservice.dto;
 
 import lombok.*;
 
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDataDto {
-
     private String correlationId;
     private String userId;
     private String username;
     private String password;
     private String email;
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 }
--- src/main/java/org/nightingaale/userservice/dto/UserProfileDto.java
+++ src/main/java/org/nightingaale/userservice/dto/UserProfileDto.java
@@ -1,29 +1,20 @@
 package org.nightingaale.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserProfileDto {
+
     @Id
     @JsonIgnore
     private String correlationId;
 
-    @JsonIgnore
     private String userId;
     private String username;
     private String info;
     private Long balance;
-
-    private LocalDateTime createdInfoAt;
-    private LocalDateTime updatedInfoAt;
 }
