41bf61c73b0a2d2a17e83d8e6c96a80682d83208
Nightingaale <shirounightingaleoff@gmail.com>
2025-08-20 22:07:43 +0300

    feature: add registered kafka event

--- src/main/java/org/nightingaale/userservice/event/UserRegisteredEvent.java
+++ src/main/java/org/nightingaale/userservice/event/UserRegisteredEvent.java
@@ -1,4 +1,14 @@
 package org.nightingaale.userservice.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegisteredEvent {
+    private String correlationId;
+    private String userId;
+    private boolean userExists;
 }
