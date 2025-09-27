1e654753b81f3797c392ebe7665fb6037ddf0b94
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-27 23:31:00 +0300

    feat: add crucial methods

--- src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
+++ src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDataRepository extends JpaRepository<UserDataEntity, String> {
     void deleteByUserId(String userId);
+    boolean existsByUsername(String username, String userId);
+    boolean existsByEmail(String email, String userId);
 }
