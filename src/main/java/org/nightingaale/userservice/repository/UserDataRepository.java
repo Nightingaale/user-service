6c38c8397a06a06a8fa80ee89d1fa64dbe11dfad
Nightingaale <shirounightingaleoff@gmail.com>
2025-10-02 23:56:04 +0300

    feature: SQL request for correlationId

--- src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
+++ src/main/java/org/nightingaale/userservice/repository/UserDataRepository.java
@@ -2,11 +2,17 @@ package org.nightingaale.userservice.repository;
 
 import org.nightingaale.userservice.model.entity.UserDataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserDataRepository extends JpaRepository<UserDataEntity, String> {
     void deleteByUserId(String userId);
     boolean existsByUsernameAndUserIdNot(String username, String userId);
     boolean existsByEmailAndUserIdNot(String email, String userId);
+
+    @Query("select u from UserDataEntity u where u.userId = ?1")
+    Optional<String> findCorrelationIdByUserId(String userId);
 }
