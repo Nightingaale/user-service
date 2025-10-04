d4587b905da2025aa2b8f3ee26f070456f52ddb1
Nightingaale <shirounightingaleoff@gmail.com>
2025-10-05 02:29:50 +0300

    feature: bean mapping annotation

--- src/main/java/org/nightingaale/userservice/mapper/UserUpdateRequestMapper.java
+++ src/main/java/org/nightingaale/userservice/mapper/UserUpdateRequestMapper.java
@@ -1,14 +1,12 @@
 package org.nightingaale.userservice.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import org.nightingaale.userservice.event.KafkaUserUpdateRequestEvent;
 import org.nightingaale.userservice.model.dto.UserDataDto;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserUpdateRequestMapper {
-    @Mapping(target = "correlationId", source = "correlationId")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "correlationId", expression = "java(correlationId)")
     KafkaUserUpdateRequestEvent toEvent(UserDataDto dataDto, String correlationId);
 }
